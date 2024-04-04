f1e167d8086870fdd156c636355da2bda544199c
will19742 <william-177@hotmail.com>
2024-04-04 11:54:25 -0600

    primera modificacion

---
+++ Account.java
@@ -0,0 +1,16 @@
+class Account {
+    Integer id;
+    String name;
+    String document;
+    String email;
+    String password;
+}
+
+
+class Account {
+    Integer id;
+    String name;
+    String document;
+    String email;
+    String password;
+}
